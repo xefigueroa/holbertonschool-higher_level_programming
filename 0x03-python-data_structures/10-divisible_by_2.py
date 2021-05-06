3707b1180a50f40e16feb67f3d0a74039e5bd28b
xefigueroa <xefigueroa@github.com>
2021-05-06 03:17:57 +0000

    syntax/logic fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     list2 = []
-    for x in my_list:
-        if x % 2 == 0:
+    for x in range(len(my_list)):
+        if my_list[x] % 2 == 0:
             list2.append(True)
         else:
             list2.append(False)
-        return list2
+    return (list2)
