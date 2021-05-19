8a523a8b7c69d0e9b0a1fb6e95dcf31db926ea3c
xefigueroa <xefigueroa@github.com>
2021-05-19 22:09:07 +0000

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Empty class square"""
+
+
+class Square:
+    """Defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initializes a square.
+
+        Args:
+            size (int): Size of a side of the square.
+        """
+        self.__size = size
