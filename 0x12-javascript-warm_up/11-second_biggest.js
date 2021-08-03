9121f919e9e79ccc9ecb8214b1d0acd43d7844f1
xefigueroa <xefigueroa@github.com>
2021-08-03 01:17:04 +0000

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,8 @@
 let secBig = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-  args.sort();
-  secBig = args[args.length - 2];
+	args.sort();
+	console.log(args[args.length - 2]);
+} else {
+	console.log(0);
 }
-console.log(secBig);
