7abaec3d01c0fc8af152663d1e0c75e550f6ac15
xefigueroa <xefigueroa@github.com>
2021-08-03 01:11:32 +0000

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const arg in args) {
   console.log(args[arg]);
 }
