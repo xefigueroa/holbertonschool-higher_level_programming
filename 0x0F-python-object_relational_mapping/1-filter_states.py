8c648670a390df5b34ce6c02bf9c0e85f483e32e
xefigueroa <xefigueroa@github.com>
2021-08-17 08:08:21 +0000

    test6

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-''' Module lists all states with a name starting with N '''
+"""Module lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    """We are documenting this here to see if passes"""
     cnx = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           psswd=sys.argv[2], db=sys.argv[3])
     c = cnx.cursor()
