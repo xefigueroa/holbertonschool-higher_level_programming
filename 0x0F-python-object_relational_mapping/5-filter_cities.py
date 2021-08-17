125f35ed0ebe1c645772c3884a455a176f714751
xefigueroa <xefigueroa@github.com>
2021-08-17 08:28:36 +0000

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""takes name of a state as argument and lists all cities of state"""
-
+"""Module takes name of a state as argument and lists all cities of state"""
 import MySQLdb
 import sys
 
@@ -9,23 +8,18 @@ if __name__ == "__main__":
     # connection
     cnx = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                           psswd=sys.argv[2], db=sys.argv[3])
-
     # cursor object creation
     c = cnx.cursor()
-
     # execution of SQL query script
-    c.execute("SELECT cities.name FROM cities
-              JOIN states ON cities.state_id=states.id
-              WHERE states.name=%s
+    c.execute("SELECT cities.name FROM cities\
+              JOIN states ON cities.state_id=states.id\
+              WHERE states.name=%s\
               ORDER BY cities.id ASC", (sys.argv[4], ))
-
     # fetch remaining rows after script executed
     query_rows = c.fetchall()
-
     # printing query
     for row in query_rows:
         print(row)
-
     # close cursor and connection
     c.close()
     cnx.close()
