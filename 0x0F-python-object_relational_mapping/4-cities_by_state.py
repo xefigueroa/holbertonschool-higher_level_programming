81cd6e1d5906d1c6a8fc2d8d801293a31a7ab6f4
xefigueroa <xefigueroa@github.com>
2021-08-19 06:40:52 +0000

    pep8 fi

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     # connection
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                          psswd=sys.argv[2], db=sys.argv[3])
+                           psswd=sys.argv[2], db=sys.argv[3])
 
     # cursor object creation
     c = conn.cursor()
