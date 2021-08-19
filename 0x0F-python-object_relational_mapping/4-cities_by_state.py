29fdbbd639fb2e0b37aec5350fc22208d8881049
xefigueroa <xefigueroa@github.com>
2021-08-19 07:05:41 +0000

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     c.execute("SELECT cities.id, cities.name, states.name\
               FROM cities JOIN states\
               WHERE cities.state_id = states.id\
-              ORDER BY cities.id")
+              ORDER BY cities.id ASC")
 
     # fetch remaining rows after script executed
     query_rows = c.fetchall()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     c.execute("SELECT cities.name FROM cities\
               JOIN states ON cities.state_id = states.id\
               WHERE states.name = %s\
-              ORDER BY cities.id", (sys.argv[4],))
+              ORDER BY cities.id ASC", (sys.argv[4],))
     # fetch remaining rows after script executed
     query_rows = c.fetchall()
     # printing query
