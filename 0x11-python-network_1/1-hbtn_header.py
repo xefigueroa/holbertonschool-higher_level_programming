acd7773611b31bd2bac3a5456915a28fe044c920
xefigueroa <xefigueroa@github.com>
2021-11-04 04:11:45 +0000

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """makes URL, sends request to URL and displays value of X-Request-Id"""
 import urllib.request as request
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as response:
