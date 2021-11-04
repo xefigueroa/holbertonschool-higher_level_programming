04c76f728aeb0d3a44b293c3fa4ed7c492812436
xefigueroa <xefigueroa@github.com>
2021-11-04 05:32:33 +0000

    type missing

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
     txt = r.text
     print("Body response:")
-    print("\t- type: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
